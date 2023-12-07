c681c95082dd595ad4044efebd11942fdd6140bd
lycfly <601753574@qq.com>
2023-12-07 11:30:13 +0800

    study boom v1

--- src/main/scala/boom_v1/Utils/replacement.scala
+++ src/main/scala/boom_v1/Utils/replacement.scala
@@ -277,20 +277,22 @@ class PseudoLRU(n_ways: Int) extends ReplacementPolicy {
 
   def perSet = true
 
-  private val state_reg = if (nBits == 0) Reg(UInt(0.W)) else RegInit(0.U(nBits.W))
+  private val state_reg = if (nBits == 0) Reg(U(0, 1 bits)) else RegInit(U(0, nBits bits))
 
-  def state_read = WireDefault(state_reg)
+  def state_read = UInt().default(state_reg)
 
   def access(touch_way: UInt): Unit = {
     state_reg := get_next_state(state_reg, touch_way)
   }
 
-  def access(touch_ways: Seq[Valid[UInt]]): Unit = {
+  def access(touch_ways: Seq[Flow[UInt]]): Unit = {
     when(touch_ways.map(_.valid).orR) {
       state_reg := get_next_state(state_reg, touch_ways)
     }
     for (i <- 1 until touch_ways.size) {
-      cover(PopCount(touch_ways.map(_.valid)) === i.U, s"PLRU_UpdateCount$i", s"PLRU Update $i simultaneous")
+     // cover(CountOne(touch_ways.map(_.valid)) === U(i), s"PLRU_UpdateCount$i", s"PLRU Update $i simultaneous")
+      cover(CountOne(touch_ways.map(_.valid)) === U(i))
+
     }
   }
 
@@ -301,30 +303,30 @@ class PseudoLRU(n_ways: Int) extends ReplacementPolicy {
    */
   def get_next_state(state: UInt, touch_way: UInt, tree_nways: Int): UInt = {
     require(state.getWidth == (tree_nways - 1), s"wrong state bits width ${state.getWidth} for $tree_nways ways")
-    require(touch_way.getWidth == (log2Ceil(tree_nways) max 1), s"wrong encoded way width ${touch_way.getWidth} for $tree_nways ways")
+    require(touch_way.getWidth == (log2Up(tree_nways) max 1), s"wrong encoded way width ${touch_way.getWidth} for $tree_nways ways")
 
     if (tree_nways > 2) {
       // we are at a branching node in the tree, so recurse
-      val right_nways: Int = 1 << (log2Ceil(tree_nways) - 1) // number of ways in the right sub-tree
+      val right_nways: Int = 1 << (log2Up(tree_nways) - 1) // number of ways in the right sub-tree
       val left_nways: Int = tree_nways - right_nways // number of ways in the left sub-tree
-      val set_left_older = !touch_way(log2Ceil(tree_nways) - 1)
-      val left_subtree_state = state.extract(tree_nways - 3, right_nways - 1)
-      val right_subtree_state = state(right_nways - 2, 0)
+      val set_left_older = !touch_way(log2Up(tree_nways) - 1)
+      val left_subtree_state = state(tree_nways - 3 downto right_nways - 1)
+      val right_subtree_state = state(right_nways - 2 downto 0)
 
       if (left_nways > 1) {
         // we are at a branching node in the tree with both left and right sub-trees, so recurse both sub-trees
         Cat(set_left_older,
           Mux(set_left_older,
             left_subtree_state, // if setting left sub-tree as older, do NOT recurse into left sub-tree
-            get_next_state(left_subtree_state, touch_way.extract(log2Ceil(left_nways) - 1, 0), left_nways)), // recurse left if newer
+            get_next_state(left_subtree_state, touch_way(log2Up(left_nways) - 1 downto 0), left_nways)), // recurse left if newer
           Mux(set_left_older,
-            get_next_state(right_subtree_state, touch_way(log2Ceil(right_nways) - 1, 0), right_nways), // recurse right if newer
+            get_next_state(right_subtree_state, touch_way(log2Up(right_nways) - 1 downto 0), right_nways), // recurse right if newer
             right_subtree_state)) // if setting right sub-tree as older, do NOT recurse into right sub-tree
       } else {
         // we are at a branching node in the tree with only a right sub-tree, so recurse only right sub-tree
         Cat(set_left_older,
           Mux(set_left_older,
-            get_next_state(right_subtree_state, touch_way(log2Ceil(right_nways) - 1, 0), right_nways), // recurse right if newer
+            get_next_state(right_subtree_state, touch_way(log2Up(right_nways) - 1 downto 0), right_nways), // recurse right if newer
             right_subtree_state)) // if setting right sub-tree as older, do NOT recurse into right sub-tree
       }
     } else if (tree_nways == 2) {
